838c5e93b7e201c18b0b744360af4ca01becf5a1
ulancho <ulan.five@gmail.com>
2021-08-07 18:35:22 +0600

    07-08

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
+import React from 'react';
 import Header from "./components/header/Header";
 import PostCard from "./components/postCard/PostCard";
 
+class App extends React.Component{
+    constructor(props) {
+        super(props);
 
-function App() {
-    return (
-        <>
-            <Header/>
-            <PostCard/>
-        </>
-    );
+        this.state = {
+            posts:[]
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3001/posts')
+            .then(response => {
+                if(response.ok){
+                    return response.json();
+                } else {
+                    alert('Произошла ошибка. Статус ошибки: '+response.status);
+                }
+            })
+            .then(data => this.setState({
+                posts:data
+            }))
+    }
+
+    render(){
+        return (
+            <>
+                <Header/>
+                {
+                    this.state.posts.map( item => <PostCard data={item} /> )
+                }
+            </>
+        );
+    }
 }
+
+
 
 export default App;
---
+++ src/components/postCard/PostCard.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './postCard.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp, faThumbsDown, faCommentAlt } from '@fortawesome/free-solid-svg-icons'
+
+class PostCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div className="postcard-blocks">
+                <div className="postcard">
+                    <div className="postcard_authors">
+                        <span className="create_name">Тестов Тест</span>
+                        <span className="create_date">31.07.2021 19:59</span>
+                    </div>
+                    <div className="postcard_img">
+                        <img className="postcard__img" src={this.props.data.image} alt=""/>
+                    </div>
+                    <div className="postcard_title">
+                        <a href="#">{this.props.data.title}</a>
+                    </div>
+                    <div className="postcard_func">
+                        <span className="mr-35"> <FontAwesomeIcon icon={faThumbsUp}  /> 5 </span>
+                        <span className="mr-35"> <FontAwesomeIcon icon={faThumbsDown} /> 2 </span>
+                        <span className="mr-35"> <FontAwesomeIcon icon={faCommentAlt}  /> 33 </span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default PostCard;
